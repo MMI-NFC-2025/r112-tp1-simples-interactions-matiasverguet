94bdea48fe1bbf827be03f0766fa8d9c739c9f74
matiasverguet <matias.verguet--bailly@edu.univ-fcomte.fr>
2025-11-07 14:38:46 +0100

    le bouton devient rouge apres un clic

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
 aMettreEnRouge.style.color = "red"
 
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+enRougeSuiteAClick.addEventListener("click", () => {
+  enRougeSuiteAClick.style.color = "red"
+})
